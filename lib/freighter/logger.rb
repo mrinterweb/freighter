47e7d421dd2781169088be72bbec67727b538803
Sean McCleary <seanmcc@gmail.com>
2014-11-03 13:26:58 -0800

    improving logger

--- lib/freighter/logger.rb
+++ lib/freighter/logger.rb
@@ -1,19 +1,24 @@
+require 'logger'
+
 module Freighter
   class Logger
+    attr_reader :logger
+
     def initialize(options)
-      @verbose = options.verbose
+      @logger = ::Logger.new(STDOUT)
+      @logger.formatter = ->(severity, time, progname, msg) { "#{severity}: #{msg}\n" }
+      logger.level = options.verbose ? ::Logger::DEBUG : ::Logger::INFO
     end
 
-    def log(str, verbose=false)
-      if @verbose or verbose != :verbose
-        puts str
-      end
+    def method_missing(meth, *args, &block)
+      logger.send(meth, *args)
     end
 
     def error(str)
-      puts str
-      puts "Freighter hit an iceburg. To the life boats. All is lost. :("
+      logger.error str
+      logger.error "Freighter hit an iceburg. To the life boats. All is lost. :("
       exit -1
     end
+     
   end
 end
