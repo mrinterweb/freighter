e912608d66b122ef1cad41def5ce3f8e726db870
Sean McCleary <seanmcc@gmail.com>
2014-11-05 16:18:01 -0800

    Fixing issue with ssh tunnel getting closed

--- lib/freighter/logger.rb
+++ lib/freighter/logger.rb
@@ -1,4 +1,5 @@
 require 'logger'
+require 'pry'
 
 module Freighter
   class Logger
--- lib/freighter/ssh.rb
+++ lib/freighter/ssh.rb
@@ -19,7 +19,7 @@ module Freighter
 
     def tunneled_proxy(local_port, use_proxy=false, &block)
       options = use_proxy ? { proxy: proxy } : @ssh_options
-      docker_port = OPTIONS.config['connection']['docker']['port']
+      docker_port = OPTIONS.config['docker']['port']
 
       LOGGER.debug "Connecting\n  host: #{@host}, user: #{@user}, options: #{options.inspect}"
       @thread = Thread.new do
@@ -27,23 +27,20 @@ module Freighter
 
         Net::SSH.start(@host, @user, options) do |session|
           session.forward.local(local_port, "0.0.0.0", docker_port)
-          LOGGER.debug "Connected to #{@host} and port #{local_port} is forwarded to host's docker REST API port."
+          LOGGER.debug "Connected to #{@host} and port #{local_port} is forwarded to host's docker REST API port ##{docker_port}."
 
           Thread.current.thread_variable_set(:ssh_tunnel_established, true)
           int_pressed = false
           trap("INT") { int_pressed = true }
           session.loop(0.1) { !session.closed? && !int_pressed }
-          Thread.current.exit
         end
       end
 
-      while thread.thread_variable_get(:ssh_tunnel_established) != true
+      while @thread.thread_variable_get(:ssh_tunnel_established) != true
         sleep 0.1
       end
 
       yield
-    ensure
-      @thread.exit
     end
 
   end
